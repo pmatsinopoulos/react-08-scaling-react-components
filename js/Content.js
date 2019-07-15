f293fd27d4f95650732d4483b6c3f1b137a9cd91
Panayotis Matsinopoulos <>
2019-07-15 15:46:05 +0300

    Hello Workd is ready

---
+++ js/Content.js
@@ -0,0 +1,13 @@
+class Content extends React.Component {
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "h1",
+        null,
+        "Hello World"
+      )
+    );
+  }
+}
---
+++ jsx/Content.jsx
@@ -0,0 +1,9 @@
+class Content extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
